bba9dd2723813fa056ff735b984e0f188d540cd6
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 21:15:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,15 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def self.create_table
+    
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
