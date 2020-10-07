61ca2c8f01ec48d04b47d11e58a679f577192d7d
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 21:29:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   end
   
   def self.create(name, grade, id) 
-    student = Student.new(name, grade)
+    student = Student.new(name, grade, id)
     student.save
     student
   end
